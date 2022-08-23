c6047c4736a341ef686d0df227f98ba85cefd987
Luciano Sanchez <luciano.sanchez1421@gmail.com>
2022-08-23 20:08:14 -0300

    Creando archivos html Y JS

---
+++ clase2-LuchoSanchez21/index.js
@@ -0,0 +1,14 @@
+console.log("Hola mundo")
+
+const miObjeto = { 
+    codigo: 100,
+    decripción: 'silla redonda'
+}
+
+console.log(miObjeto)
+
+function calcular(param1, param2) {
+    return param1 + param2
+}
+
+console.log(calcular(2000, 22))
