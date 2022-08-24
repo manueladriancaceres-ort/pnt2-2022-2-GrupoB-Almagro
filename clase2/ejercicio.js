745d7ff894adc241c152563e0fe7ca1d463a1b31
Juan Carlos Suarez <caisuarez@gmail.com>
2022-08-23 23:11:40 -0300

    cuarto commit

--- clase2/ejercicio.js
+++ clase2/ejercicio.js
@@ -7,14 +7,52 @@ const facturas = [
 ]
 
 // consignas:
-
-// 1 - listar facturas
-// 2 - ordenar codigo en un nuevo arreglo
-// 3 - ordenar por importe en un nuevo arreglo
+// 1 - listar facturas - OK
+// 2 - ordenar por codigo en un nuevo arreglo - OK
+// 3 - ordenar por importe en un nuevo arreglo - OK
 // 4 - suma de importes
 // 5 - nueva lista importes mayores a 7000
 // 6 - suma de importe con facturas mayores a 7000
 // 7 - bonus: lista de facturas con diferencia
 //     (entre importe y suma de items)
 
+//1
 facturas.forEach(factura => console.log(factura));
+
+//2
+const lista = [...facturas];
+/*
+function comparar_codigo(a,b) {
+    if (a.codigo < b.codigo){
+        return -1;
+    }
+    if (a.codigo > b.codigo){
+        return 1;
+    }
+    return 0;
+}
+const nuevaLista = lista.sort(comparar_codigo);
+nuevaLista.forEach(factura => console.log(factura));
+*/
+const listaOrdenadaXCodigo = lista.sort((a,b) => a.codigo - b.codigo);
+console.log(listaOrdenadaXCodigo);
+//listaOrdenadaXCodigo.forEach(factura => console.log(factura));
+
+//3
+const lista2 = [...facturas];
+/*
+function comparar_importe(a,b) {
+    if (a.importe < b.importe){
+        return -1;
+    }
+    if (a.importe > b.importe){
+        return 1;
+    }
+    return 0;
+}
+const listaOrdenadaXImporte = lista2.sort(comparar_importe);
+*/
+const listaOrdenadaXImporte = lista2.sort((a,b) => a.importe - b.importe);
+console.log(listaOrdenadaXImporte);
+//nuevaLista2.forEach(factura => console.log(factura));
+
