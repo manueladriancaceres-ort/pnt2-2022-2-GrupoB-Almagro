6da779577dcb7005220af4dbd1db5565fddf8a30
schargo7 <schargo7@gmail.com>
2022-09-06 20:36:26 -0300

    trabajo 6 del 9

---
+++ clase4/index.js
@@ -0,0 +1,31 @@
+const obj = Vue.createApp({
+    data() {
+        return {
+            factura: {
+                titulo: 'Titulo Factura', codigo: 5, nroCliente: 3, items: [{ descripcion: 'null' }]
+            },
+            myStyle: { color: 'green', fontSize: '50px' },
+        }
+    },
+
+
+    methods: {
+        agregarItem() {
+            this.factura.items.push({ descripcion: this.factura.items.descripcion })
+        },
+        cambiarColorTitulo() {
+            if (this.myStyle.color == 'green') {
+                this.myStyle.color = 'red';
+            } else {
+                this.myStyle.color = 'green';
+            }
+        },
+        ordenarXDescripcion() {
+            this.factura.items.sort((a, b) => a.descripcion.localeCompare(b.descripcion));
+        },
+        mostrarFactura(){
+            console.log(this.factura)
+            this.factura.items.forEach(item => {console.log(item)})
+        }
+    }
+}).mount('#app')
