ad2462d3a3270bf8dc8f832de2bd3e623278550b
Gabbiarce <gabrielarce96@gmail.com>
2022-09-13 22:57:03 -0300

    clase 5

---
+++ ejercicio1/src/views/Detalle.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      Alta de usuarios: <br>
+      Nombre:   <input type="text" v-model="usuario.nombre"><br>
+      Proyecto: <input type="text"  v-model="usuario.proyecto"><br>
+      <button @click="agregar">Agregar usuario</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+      data() {
+        return{
+            usuario : {}
+        }
+      },
+      methods: {
+        agregar(){
+            this.$emit("agregar", {...this.usuario});
+        }
+      }
+  
+  }
+  </script>
+  
+  <style>
+  
+  </style>
+  
---
+++ ejercicio1/src/views/Sistema.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+      <h2>Sistema de usuarios</h2>
+      <li v-for="usuario in usuarios">
+        {{usuario.numero}} {{usuario.nombre}} {{usuario.proyecto}}
+      </li>
+      <Detalle @agregar="agregar($event)"/>
+    </div>
+</template>
+  
+  <script>
+  import Detalle from './Detalle.vue'
+  export default {
+    components: { Detalle},
+    data() {
+        return{
+            usuarios: []
+        }
+    },
+    methods:{
+        agregar(usuario){
+            usuario.numero = this.conseguirMayor()+1
+            this.usuarios.push(usuario)
+        },
+        conseguirMayor(){
+            let mayor = 0;
+            this.usuarios.forEach(u => {
+                if (u.numero > mayor) {
+                    mayor = u.numero;
+                }
+            });
+            return mayor;
+        }
+    }
+  }
+  </script>
+  
+  <style>
+  
+  </style>
+  
